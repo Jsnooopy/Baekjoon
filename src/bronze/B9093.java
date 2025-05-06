6ddca905c29b90580f0184e754a35962ebe67e33
재 이 <lj3552@o365.yonsei.ac.kr>
2025-05-07 06:46:22 +0900

    단어 뒤집기

--- src/bronze/B9093.java
+++ src/bronze/B9093.java
@@ -6,12 +6,13 @@ import java.util.Stack;
 public class B9093 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        Stack<Character> stack = new Stack<>();
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        while(t-- > 0) {
-            String s = br.readLine()+"\n";
+        int T = Integer.parseInt(br.readLine());
+        Stack<Character> stack = new Stack<>();
+
+        while(T-- > 0) {
+            String s = br.readLine() + "\n";
             for(char c : s.toCharArray()) {
                 if(c == ' ' || c == '\n') {
                     while(!stack.isEmpty()) {
