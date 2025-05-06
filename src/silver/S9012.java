3b8f27222b4b4f292666bf4cb44439d82d5c10e1
재 이 <lj3552@o365.yonsei.ac.kr>
2025-05-07 07:17:10 +0900

    괄호

---
+++ src/silver/S9012.java
@@ -0,0 +1,40 @@
+package silver;
+
+import java.io.*;
+import java.util.Stack;
+
+public class S9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            Stack<Character> stack = new Stack<>();
+            String s = br.readLine();
+            boolean b = true;
+
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == '(') {
+                    stack.push('(');
+                } else {
+                    if (stack.empty()) {
+                        b = false;
+                        break;
+                    }
+                    stack.pop();
+                }
+            }
+            if (!stack.empty()) {
+                b = false;
+            }
+
+            if (b) {
+                bw.write("YES\n");
+            } else {
+                bw.write("NO\n");
+            }
+        }
+        bw.flush();
+    }
+}
