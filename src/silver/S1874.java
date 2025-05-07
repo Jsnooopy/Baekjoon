f9b503a7b1d0179e794a4d58487cbfe482fb7c17
재 이 <lijae3552@gmail.com>
2025-05-07 09:38:15 +0900

    스택 수열

---
+++ src/silver/S1874.java
@@ -0,0 +1,35 @@
+package silver;
+
+import java.io.*;
+import java.util.Stack;
+
+public class S1874 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        int top = 0;    //stack 가장 위에 있는 값
+
+        while (N > 0) {
+            int n = Integer.parseInt(br.readLine());
+
+            if (n > top) {
+                for (int i = top + 1; i <= n; i++) {
+                    stack.push(i);
+                    sb.append('+').append('\n');
+                }
+                top = n;
+            } else if (stack.peek() != n) {
+                System.out.println("NO");
+                return;
+            }
+            stack.pop();
+            sb.append('-').append('\n');
+            N--;
+        }
+        System.out.println(sb);
+    }
+}
