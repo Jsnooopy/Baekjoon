3d1dd33bafdbb470e290f7c48e348454e3ad3b16
재 이 <lijae3552@gmail.com>
2025-05-25 16:54:13 +0900

    카드 구매하기 2

---
+++ src/silver/S16194.java
@@ -0,0 +1,32 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S16194 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N + 1];
+        int[] dp = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = arr[i];
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j] + arr[j]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
