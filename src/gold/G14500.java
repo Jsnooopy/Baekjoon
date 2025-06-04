355f33c563f7d2a7518957e76b16264256f80531
재 이 <lijae3552@gmail.com>
2025-06-04 19:14:46 +0900

    테트로미노

---
+++ src/gold/G14500.java
@@ -0,0 +1,13 @@
+package gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class G14500 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
