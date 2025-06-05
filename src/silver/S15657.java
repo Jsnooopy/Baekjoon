e917392961b1dedaaad2d14a860dab8faa7984dd
재 이 <lijae3552@gmail.com>
2025-06-06 00:49:10 +0900

    N과 M 8

---
+++ src/silver/S15657.java
@@ -0,0 +1,11 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S15657 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
