01c9fcbb20d78217e5a7084238a14c1d4237562c
재 이 <lijae3552@gmail.com>
2025-06-08 20:32:13 +0900

    N과 M 8

--- src/silver/S15657.java
+++ src/silver/S15657.java
@@ -3,9 +3,45 @@ package silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class S15657 {
+    public static int N, M;
+    public static int[] arr, print;
+    public static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        print = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int at, int depth) {
+        if (depth == M) {
+            for (int val : print) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = at; i < N; i++) {
+            print[depth] = arr[i];
+            dfs(i, depth + 1);
+        }
     }
 }
