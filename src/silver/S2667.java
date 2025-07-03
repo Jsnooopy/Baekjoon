c1b85004790ac8a3c15d33ea3937ccde30a6ba30
재 이 <lijae3552@gmail.com>
2025-07-04 02:03:02 +0900

    단지 번호 붙이기

---
+++ src/silver/S2667.java
@@ -0,0 +1,13 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S2667 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
