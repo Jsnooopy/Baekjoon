d85369633eff591b4b7283ffe238eace42aeb830
재 이 <lijae3552@gmail.com>
2025-07-28 01:52:58 +0900

    카드 2

---
+++ src/silver/S2164.java
@@ -0,0 +1,27 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class S2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
+
+        while (q.size() > 1) {
+            q.poll();
+            q.add(q.poll());
+        }
+
+        System.out.println(q.peek());
+    }
+}
