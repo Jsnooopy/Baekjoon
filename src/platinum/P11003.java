c2f32073bd02ea5ade50e769da47620a207d2f87
재 이 <lijae3552@gmail.com>
2025-07-27 22:16:36 +0900

    최솟값 찾기

---
+++ src/platinum/P11003.java
@@ -0,0 +1,13 @@
+package platinum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P11003 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
