2c97bbfd273cc8b2447565381fa35dca9ac97176
DearJ <108777654+Jsnooopy@users.noreply.github.com>
2025-09-03 15:49:53 +0900

    경로 찾기

---
+++ src/silver/S11403.java
@@ -0,0 +1,46 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S11403 {
+	static int N;
+	static int[][] distance;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		distance = new int[N][N];
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				distance[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int k = 0; k < N; k++) {
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(distance[i][k] == 1 && distance[k][j] == 1) distance[i][j] = 1;
+				}
+			}
+		}
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				sb.append(distance[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+}
