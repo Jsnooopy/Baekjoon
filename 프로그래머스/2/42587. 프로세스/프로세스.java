4875fcc01c89cc845f0880ef9b6a752f1f596360
Jae Lee <lijae3552@gmail.com>
2026-04-21 16:14:19 +0900

    [level 2] Title: 프로세스, Time: 1.73 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    
+    static class Process {
+        int idx, priority;
+        
+        Process(int idx, int priority){
+            this.idx = idx;
+            this.priority = priority;
+        }
+    }
+    
+    public int solution(int[] priorities, int location) {
+        Queue<Process> q = new ArrayDeque<>();
+        
+        for(int i = 0; i < priorities.length; i++){
+            q.offer(new Process(i, priorities[i]));
+        }
+        
+        int answer = 0;
+        
+        while(!q.isEmpty()){
+            Process now = q.poll();
+            boolean check = false;
+            
+            for(Process next : q){
+                if(next.priority > now.priority){
+                    check = true;
+                    break;
+                }
+            }
+            
+            if(check){
+                q.offer(now);
+            }else{
+                answer++;
+                
+                if(now.idx == location) return answer;
+            }
+        }
+        
+        return answer;
+    }
+}
